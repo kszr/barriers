0ec406fde3625531037b7239dcab7907ca44b86e
kszr <abhishek.rob@gmail.com>
2016-02-27 20:25:43 -0500

    MCS possibly working. Not sure what to do about 'sense' variable.'

--- mpi_mcs.c
+++ mpi_mcs.c
@@ -19,40 +19,45 @@ static int sense = 1;
 static int num_procs;
 
 void tree_barrier(treenode_t *treenode) {
-    int buf;
-    MPI_Request mpi_request;
+    MPI_Status mpi_status;
+    int childbuffer[4]; // Buffer that each child in the arrival tree uses to signal this node.
+    int buf; // Buffer for other signals.
     int i;
 
     // Spin until all children are ready.
     while(!children_not_ready(treenode)) {
         // Not shared memory, so the parent has to receive the request and proceed accordingly.
+        int i;
         for(i=0; i<4; i++) {
             if(treenode->childid_arrive[i] != -1)
-                MPI_Irecv(&buf, 1, MPI_INT, treenode->childid_arrive[i], 1, MPI_COMM_WORLD, &mpi_request);
-            if(buf == I_M_READY)
+                MPI_Recv(&childbuffer[i], 1, MPI_INT, treenode->childid_arrive[i], 1, MPI_COMM_WORLD, &mpi_status);
+
+            if(childbuffer[i] == I_AM_READY) {
+                printf("Process %d heard back from %d\n", treenode->processid, treenode->childid_arrive[i]);
                 treenode->childnotready[i] = 0;
+            }
         }
     }
 
     printf("Processor %d: All my children are ready.\n", treenode->processid);
 
     // Signal to parent that I am ready.
-    buf = I_M_READY;
     if(treenode->processid != 0) {
+        childbuffer[(treenode->processid - 1) % 4] = I_AM_READY;
         int parentid_a = (treenode->processid-1)/4;
-        MPI_Send(&buf, 1, MPI_INT, parentid_a, 1, MPI_COMM_WORLD);
-        printf("Processor %d: Signaled I_M_READY to my parent.\n", treenode->processid);
+        MPI_Send(&childbuffer[(treenode->processid - 1) % 4], 1, MPI_INT, parentid_a, 1, MPI_COMM_WORLD);
     }
 
     // Reset childnotready array in preparation for the next barrier.
     for(i=0; i<4; i++)
-        treenode->childnotready[i] = 4*treenode->processid+i < num_procs ? 1 : -1;
+        treenode->childnotready[i] = 4*treenode->processid+i+1 < num_procs ? 1 : -1;
 
     // If I am not the root, spin until my parent signals wakeup.
     if(treenode->processid != 0) {
         while(1) {
+            buf = -1;
             int parentid_w = (treenode->processid-1)/2; // Different parent for wakeup;
-            MPI_Irecv(&buf, 1, MPI_INT, parentid_w, 1, MPI_COMM_WORLD, &mpi_request);
+            MPI_Recv(&buf, 1, MPI_INT, parentid_w, 1, MPI_COMM_WORLD, &mpi_status);
             if(buf == WAKEUP_SIGNAL)
                 break;
         }
@@ -64,9 +69,7 @@ void tree_barrier(treenode_t *treenode) {
     if(treenode->childid_wakeup[1] != -1)
         MPI_Send(&buf, 1, MPI_INT, treenode->childid_wakeup[1], 1, MPI_COMM_WORLD);
 
-    printf("Processor %d: Sent wakeup signal to children.\n", treenode->processid);
-
-    sense = !sense;
+    sense = !sense; // May not be using sense at all.
 }
 
 /**
@@ -105,13 +108,12 @@ int main(int argc, char *argv[]) {
 
     int i;
     for(i=0; i<4; i++) {
-        treenode.childid_arrive[i] = 4*id+i < num_procs;
-        treenode.childnotready[i] = 4*id+i < num_procs ? 1 : -1;
+        treenode.childid_arrive[i] = 4*id+i+1 < num_procs ? 4*id+i+1 : -1;
+        treenode.childnotready[i] = 4*id+i+1 < num_procs ? 1 : -1;
     }
 
-    for(i=0; i<2; i++) {
+    for(i=0; i<2; i++)
         treenode.childid_wakeup[i] = 2*id+i+1 < num_procs ? 2*id+i+1 : -1; 
-    }
         
     wtime = MPI_Wtime();
 
--- mpi_mcs.h
+++ mpi_mcs.h
@@ -4,13 +4,9 @@
 #ifndef MPI_MCS_H
 #define MPI_MCS_H
 
-static const int I_M_READY = 0;
+// Signals
+static const int I_AM_READY = 0;
 static const int WAKEUP_SIGNAL = 1;
-
-typedef struct processor_s {
-    int id;
-    int is_ready;
-} processor_t;
 
 typedef struct treenode_s {
     int processid;
@@ -18,7 +14,6 @@ typedef struct treenode_s {
     int childid_wakeup[2]; // Stores the id of its two children in the wakeup tree.
     int childid_arrive[4]; // Stores the id of its four children in the arrival tree; -1 if there is no child at this position.
     int childnotready[4]; // Stores child id (if any) and: -1 if no corresponding child; 0 if child not ready; 1 if child is ready. 
-    int dummy; // Pseudo data (?)
 } treenode_t;
 
 void tree_barrier(treenode_t *treenode);
