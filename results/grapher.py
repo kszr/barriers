5f375c391871b1d32ed0126dc15bcf4b7f788dc8
kszr <abhishek.rob@gmail.com>
2016-03-06 11:37:59 -0500

    Adding tentative graph for combined

--- results/grapher.py
+++ results/grapher.py
@@ -22,11 +22,37 @@ for line in file:
         tournament_vals.append(float(m.groups()[1]))
 tournament_results = np.array(tournament_vals)
 
-x = np.arange(2.0, 13.0)
 
+"""
+Plotting MPI MCS and Tournament Barriers.
+"""
+
+x = np.arange(2.0, 13.0)
 plt.plot(x, mcs_results, "o-", label="MCS")
 plt.plot(x, tournament_results, "s-", label="Tournament")
 plt.xlabel("Number of Processors")
 plt.ylabel("Time in Microsecs")
-legend(('MCS','Tournament'))
+plt.legend(("MCS","Tournament"), loc="upper left")
 plt.savefig("MCS_Tourney.png")
+
+mpi_omp_vals = []
+file = open("mpi_omp_final.txt", "r")
+for line in file:
+    p = re.compile("([0-9]+): 12: ([0-9]+.[0-9]+)")
+    m = p.search(line)
+    if m:
+        mpi_omp_vals.append(float(m.groups()[1]))
+mpi_omp_results = np.array(mpi_omp_vals)
+
+"""
+Plotting MPI-OMP with 12 threads per process against MPI MCS and MPI Tournament.
+"""
+plt.cla()
+x = np.arange(2.0, 9.0)
+plt.plot(x, np.array(mcs_vals[0:7]), "o-", label="MCS")
+plt.plot(x, np.array(tournament_vals[0:7]), "s-", label="Tournament")
+plt.plot(x, mpi_omp_results, "^-", label="MPI OMP")
+plt.xlabel("Number of Processors")
+plt.ylabel("Time in Microsecs")
+plt.legend(("MCS", "Tournament", "MPI-OMP"), loc="upper left")
+plt.savefig("MPI_OMP.png")
