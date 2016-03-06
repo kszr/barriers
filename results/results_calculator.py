96f0b108c8e500a7c5605043a05a607215536d32
kszr <abhishek.rob@gmail.com>
2016-03-06 02:57:39 -0500

    Updating results script for mpi_omp_barrier and updloading reduced results for mpi_omp_barrier

--- results/results_calculator.py
+++ results/results_calculator.py
@@ -50,3 +50,34 @@ for key in mcs_map:
     file.write(str(key) + ": " + str(10000*float(mcs_map[key][0])/int(mcs_map[key][1])));
     file.write("\n");
 file.close()
+
+
+"""
+MPI OMP results extracted below. All results in microseconds.
+"""
+mpi_omp_map = {}
+dir_mpi_omp = "mpi_omp_results"
+mpi_omp_files = os.listdir(dir_mpi_omp)
+for path in mpi_omp_files:
+    file = open(dir_mpi_omp + "/" + path, "r")
+    for line in file:
+        p = re.compile("took (0.[0-9]*)s to run with ([0-9]*) processors and ([0-9]*) threads")
+        m = p.search(line)
+        if m:
+            try:
+                mpi_omp_map[int(m.groups()[1])][int(m.groups()[2])] = (float(m.groups()[0])+mpi_omp_map[int(m.groups()[1])][int(m.groups()[2])][0], 1+int(mpi_omp_map[int(m.groups()[1])][int(m.groups()[2])][1]))
+            except KeyError:
+                try:
+                    mpi_omp_map[int(m.groups()[1])][int(m.groups()[2])] = (float(m.groups()[0]),1)
+                except KeyError:
+                    mpi_omp_map[int(m.groups()[1])] = {}
+                    mpi_omp_map[int(m.groups()[1])][int(m.groups()[2])] = (float(m.groups()[0]),1)
+
+file = open("mpi_omp_final.txt", "w")
+file.write("MPI OMP Results\n")
+file.write("(num processors: num threads: time in microseconds)\n")
+for key1 in mpi_omp_map:
+    for key2 in mpi_omp_map[key1]:
+        file.write(str(key1) + ": " + str(key2) + ": " + str(10000*float(mpi_omp_map[key1][key2][0])/int(mpi_omp_map[key1][key2][1])));
+        file.write("\n");
+file.close()
