240bca11acfbec73f6a9f2a12cdbb38fafcecfad
patil-abhishek <patil.abhishek1609@gmail.com>
2016-03-07 02:13:24 -0500

    updated critical section in sense reversal barrier

--- omp_sense.c
+++ omp_sense.c
@@ -12,16 +12,20 @@ static int num_barriers;
 static int count;
 
 void central_barrier (int* local_sense){
-	*local_sense = ! *local_sense;
+	*local_sense = !*local_sense;
 	
 	#pragma omp critical
+	{
 		count -= 1;
 	
 	if (count == 0) {
 		count = num_procs;
 		sense = *local_sense;
-	} else
-		while (sense != *local_sense);
+		return;
+	}
+	}
+	
+	while (sense != *local_sense) {}
 }
 
 int main(int argc, char* argv[]) {
@@ -35,22 +39,18 @@ int main(int argc, char* argv[]) {
 		}
 	
 	omp_set_num_threads(num_procs);
-	//struct timeval  T1, T2;
 	double T1, T2;
-	/*Parallel Code started*/
+	
 	#pragma omp parallel shared (sense, count)
 	{
 		int thread_id = omp_get_thread_num();
 		int local_sense = 1;
 		long i;
 		T1 = omp_get_wtime();
-		//gettimeofday(&T1, NULL);
 		for(i=0; i<num_barriers; i++) {
 			central_barrier(&local_sense);
 		}
 		T2 = omp_get_wtime();
-		//gettimeofday(&T2, NULL);
-		//printf("Thread %d spent time= %f \n", thread_id, elapsedTime(T1, T2));
 		printf("took %fs to run with %d processors\n", (T2-T1), thread_id);
 	}
 
