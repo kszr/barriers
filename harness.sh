586c1afafdac36f09f39ba5348a4e4f149c3ed3d
patil-abhishek <patil.abhishek1609@gmail.com>
2016-03-09 17:26:01 -0500

    test harness script added

---
+++ harness.sh
@@ -0,0 +1,61 @@
+
+set -e
+make -f Makefile
+
+echo "OMP Centralized Sense Reversal Barrier"
+for i in {2..8}
+do
+	for j in 1
+	do
+		./omp_sense $i $j
+		echo ""
+	done
+done
+
+read -p "Press [Enter] key to run Dissemination Barrier..."
+
+echo "OMP Dissemination Barrier"
+for i in {2..8}
+do
+	for j in 1
+	do
+		./omp_dissemination $i $j
+		echo ""
+	done
+done
+
+read -p "Press [Enter] key to run Tournament Barrier..."
+
+echo "MPI Tournament Barrier"
+for i in {2..12}
+do
+	for j in 1
+	do
+		mpirun -np $i ./mpi_tournament -i $j
+		echo ""
+	done
+done
+
+read -p "Press [Enter] key to run MCS tree Barrier..."
+
+echo "MPI MCS tree Barrier"
+for i in {2..12}
+do
+	for j in 1
+	do
+		mpirun -np $i ./mpi_mcs -i $j
+		echo ""
+	done
+done
+
+read -p "Press [Enter] key to run MPI-OMP combined Barrier..."
+
+echo "MPI-OMP Combined Barrier"
+for i in {2..12}
+do
+	for j in {2..8}
+	do
+		mpirun -np $i ./mpi_omp_barrier -i $j
+		echo ""
+	done
+done
